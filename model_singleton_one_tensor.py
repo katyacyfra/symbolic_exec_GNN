9fa82af352e9d6a6dae256ba24ba39e359b723cc
katyacyfra <katyacyfra@gmail.com>
2023-08-19 22:50:57 +0300

    fixed dimension tensor model for onxx

--- model_singleton_one_tensor.py
+++ model_singleton_one_tensor.py
@@ -16,90 +16,92 @@ class StateModelEncoder(torch.nn.Module):
         self.sizes_dict = {}
         self.vertices_n = x_dict['game_vertex'].size()[0]
         self.states_n = x_dict['state_vertex'].size()[0]
-        vertex_to_vertex_n = edge_index_dict[('game_vertex', 'to', 'game_vertex')].size()[1]
-        state_to_state_n = edge_index_dict[('state_vertex', 'parent_of', 'state_vertex')].size()[1]
-        vertex_state_history_n = edge_attr[('game_vertex', 'history', 'state_vertex')].size()[0]
-        vertex_state_in_n = edge_index_dict[('game_vertex', 'in', 'state_vertex')].size()[1]
+        vertex_to_vertex_n = edge_index_dict['game_vertex to game_vertex'].size()[1]
+        state_to_state_n = edge_index_dict['state_vertex parent_of state_vertex'].size()[1]
+        vertex_state_history_n = edge_attr['game_vertex history state_vertex'].size()[0]
+        vertex_state_in_n = edge_index_dict['game_vertex in state_vertex'].size()[1]
 
+        #fixed rows number
+        rows_n = x_dict['game_vertex'].size()[1] + x_dict['state_vertex'].size()[1] + 14
         # calculate final tensor sizes
         if fixed_size_tensor:
             columns_n = 10000
-            rows_n = 10000
         else:
 
-            columns_n = max(x_dict['game_vertex'].size()[1], x_dict['state_vertex'].size()[1],
+            columns_n = max(self.states_n, self.vertices_n,
                         vertex_to_vertex_n, state_to_state_n, vertex_state_history_n,
                         vertex_state_in_n)
-            rows_n = self.vertices_n + self.states_n + 2*len(edge_index_dict) + 2
 
         final_tensor = torch.zeros(rows_n, columns_n)
-
-        self.sizes_dict[('game_vertex')] = (0, x_dict['game_vertex'].size()[1])
+        game_param_n = x_dict['game_vertex'].size()[1]
+        self.sizes_dict['game_vertex'] = (0, game_param_n)
         row_start = 0
-        final_tensor[row_start : self.vertices_n, : x_dict['game_vertex'].size()[1]] = x_dict['game_vertex']
-        row_start += self.vertices_n
+        final_tensor[row_start : game_param_n, : self.vertices_n] = x_dict['game_vertex'].transpose(0, 1)
+        row_start += game_param_n
 
-        self.sizes_dict[('state_vertex')] = (row_start, x_dict['state_vertex'].size()[1])
-        final_tensor[row_start : row_start + self.states_n, : x_dict['state_vertex'].size()[1]] = x_dict['state_vertex']
-        row_start += self.states_n
+        state_param_n = x_dict['state_vertex'].size()[1]
+        self.sizes_dict['state_vertex'] = (row_start, state_param_n)
+        final_tensor[row_start : row_start + state_param_n, :self.states_n] = x_dict['state_vertex'].transpose(0, 1)
+        row_start += state_param_n
 
-        self.sizes_dict[('game_vertex', 'history', 'state_vertex')] = (row_start, vertex_state_history_n)
-        final_tensor[row_start : row_start + 2, : vertex_state_history_n] = edge_index_dict[('game_vertex', 'history', 'state_vertex')]
+        self.sizes_dict['game_vertex history state_vertex'] = (row_start, vertex_state_history_n)
+        final_tensor[row_start : row_start + 2, : vertex_state_history_n] = edge_index_dict['game_vertex history state_vertex']
         row_start += 2
 
-        self.sizes_dict[('attr', 'game_vertex', 'history', 'state_vertex')] = (row_start, vertex_state_history_n)
-        final_tensor[row_start : row_start + 1, : vertex_state_history_n] = edge_attr[('game_vertex', 'history', 'state_vertex')].transpose(0, 1)
+        self.sizes_dict['attr game_vertex history state_vertex'] = (row_start, vertex_state_history_n)
+        final_tensor[row_start : row_start + 1, : vertex_state_history_n] = edge_attr[ 'game_vertex history state_vertex'].transpose(0, 1)
         row_start += 1
 
-        self.sizes_dict[('state_vertex', 'history', 'game_vertex')] = (row_start, vertex_state_history_n)
-        final_tensor[row_start : row_start + 2, : vertex_state_history_n] = edge_index_dict[('state_vertex', 'history', 'game_vertex')]
+        self.sizes_dict['state_vertex history game_vertex'] = (row_start, vertex_state_history_n)
+        final_tensor[row_start : row_start + 2, : vertex_state_history_n] = edge_index_dict['state_vertex history game_vertex']
         row_start += 2
 
-        self.sizes_dict[('attr', 'state_vertex', 'history', 'game_vertex')] = (row_start, vertex_state_history_n)
-        final_tensor[row_start : row_start + 1, : vertex_state_history_n] = edge_attr[('state_vertex', 'history', 'game_vertex')].transpose(0, 1)
+        self.sizes_dict['attr state_vertex history game_vertex'] = (row_start, vertex_state_history_n)
+        final_tensor[row_start : row_start + 1, : vertex_state_history_n] = edge_attr['state_vertex history game_vertex'].transpose(0, 1)
         row_start += 1
 
-        self.sizes_dict[('game_vertex', 'in', 'state_vertex')] = (row_start, vertex_state_in_n)
-        final_tensor[row_start : row_start + 2, : vertex_state_in_n] = edge_index_dict[('game_vertex', 'in', 'state_vertex')]
+        self.sizes_dict['game_vertex in state_vertex'] = (row_start, vertex_state_in_n)
+        final_tensor[row_start : row_start + 2, : vertex_state_in_n] = edge_index_dict['game_vertex in state_vertex']
         row_start += 2
 
-        self.sizes_dict[('state_vertex', 'in', 'game_vertex')] = (row_start, vertex_state_in_n)
-        final_tensor[row_start : row_start + 2, : vertex_state_in_n] = edge_index_dict[('state_vertex', 'in', 'game_vertex')]
+        self.sizes_dict['state_vertex in game_vertex'] = (row_start, vertex_state_in_n)
+        final_tensor[row_start : row_start + 2, : vertex_state_in_n] = edge_index_dict['state_vertex in game_vertex']
         row_start += 2
 
-        self.sizes_dict[('game_vertex', 'to', 'game_vertex')] = (row_start, vertex_to_vertex_n)
-        final_tensor[row_start : row_start + 2, : vertex_to_vertex_n] = edge_index_dict[('game_vertex', 'to', 'game_vertex')]
+        self.sizes_dict['game_vertex to game_vertex'] = (row_start, vertex_to_vertex_n)
+        final_tensor[row_start : row_start + 2, : vertex_to_vertex_n] = edge_index_dict['game_vertex to game_vertex']
         row_start += 2
 
-        self.sizes_dict[('state_vertex', 'parent_of', 'state_vertex')] = (row_start, state_to_state_n)
-        final_tensor[row_start : row_start + 2, : state_to_state_n] = edge_index_dict[('state_vertex', 'parent_of', 'state_vertex')]
+        state_to_state = edge_index_dict['state_vertex parent_of state_vertex']
+        self.sizes_dict['state_vertex parent_of state_vertex'] = (row_start, state_to_state_n)
+        final_tensor[row_start : row_start + 2, : state_to_state_n] = edge_index_dict['state_vertex parent_of state_vertex']
         return final_tensor
 
 
     def forward(self, x):
-        game_x = x[self.sizes_dict[('game_vertex')][0]:self.vertices_n,
-                 :self.sizes_dict[('game_vertex')][1]]
-        start = self.sizes_dict[('game_vertex', 'to', 'game_vertex')][0]
+        game_x = x[self.sizes_dict['game_vertex'][0]:self.sizes_dict['game_vertex'][1],
+                 :self.vertices_n].transpose(0,1)
+        start = self.sizes_dict['game_vertex to game_vertex'][0]
         vertex_to_vertex = x[start : start + 2,
-                 :self.sizes_dict[('game_vertex', 'to', 'game_vertex')][1]].to(torch.int64)
+                 :self.sizes_dict['game_vertex to game_vertex'][1]].to(torch.int64)
 
         game_x = self.conv1(
             game_x,
             vertex_to_vertex,
         ).relu()
 
-        start = self.sizes_dict[('state_vertex')][0]
+        start = self.sizes_dict['state_vertex'][0]
 
 
-        state_x = x[start: start + self.states_n,
-                 :self.sizes_dict[('state_vertex')][1]]
+        state_x = x[start: start + self.sizes_dict['state_vertex'][1],
+                 :self.states_n].transpose(0,1)
 
-        start = self.sizes_dict[('game_vertex', 'history', 'state_vertex')][0]
+        start = self.sizes_dict['game_vertex history state_vertex'][0]
         history_index = x[start : start + 2,
-                 :self.sizes_dict[('game_vertex', 'history', 'state_vertex')][1]].to(torch.int64)
+                 :self.sizes_dict['game_vertex history state_vertex'][1]].to(torch.int64)
         start += 2
         history_attr = x[start : start + 1,
-                 :self.sizes_dict[('attr', 'game_vertex', 'history', 'state_vertex')][1]].transpose(0,1)
+                 :self.sizes_dict['attr game_vertex history state_vertex'][1]].transpose(0,1)
 
         state_x = self.conv3(
             (game_x, state_x),
@@ -107,22 +109,24 @@ class StateModelEncoder(torch.nn.Module):
             history_attr,
         ).relu()
 
-        start = self.sizes_dict[('game_vertex', 'in', 'state_vertex')][0]
+        start = self.sizes_dict['game_vertex in state_vertex'][0]
         in_index = x[start : start + 2,
-                 :self.sizes_dict[('game_vertex', 'in', 'state_vertex')][1]].to(torch.int64)
+                 :self.sizes_dict['game_vertex in state_vertex'][1]].to(torch.int64)
 
         state_x = self.conv4(
             (game_x, state_x),
             in_index,
         ).relu()
 
-        start = self.sizes_dict[('state_vertex', 'parent_of', 'state_vertex')][0]
-        parent_index = x[start : start + 2,
-                 :self.sizes_dict[('state_vertex', 'parent_of', 'state_vertex')][1]].to(torch.int64)
+        state_edges_n = self.sizes_dict['state_vertex parent_of state_vertex'][1]
+        if state_edges_n != 0:
+            start = self.sizes_dict['state_vertex parent_of state_vertex'][0]
+            parent_index = x[start : start + 2,
+                 :state_edges_n].to(torch.int64)
 
-        state_x = self.conv2(
-            state_x,
-            parent_index,
-        ).relu()
+            state_x = self.conv2(
+               state_x,
+               parent_index,
+            ).relu()
 
         return self.lin(state_x)
